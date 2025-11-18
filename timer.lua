1600c4350b1f234043d43876b9ab59ca453610dc
CoderDan67 <daniel.lipcak@gmail.com>
2025-11-18 18:41:03 +0100

    Add files via upload

---
+++ timer.lua
@@ -0,0 +1,29 @@
+local timer = {}
+timer.list = {}
+
+function timer.newTimer(name)
+    table.insert(timer.list, 1, {name = name, time = 0})
+end    
+
+function timer.clear(name)
+    for i = #timer.list, 1, -1 do
+        if timer.list[i].name == name then
+            table.remove(timer.list, i)
+        end
+    end     
+end    
+
+function timer.wait(dt, waitTime, name)
+    for i = #timer.list, 1, -1 do
+        if timer.list[i].name == name then
+            timer.list[i].time = timer.list[i].time + dt
+            if timer.list[i].time >= waitTime then
+                timer.clear(timer.list[i].name)
+                return true
+            end
+        end 
+    end    
+    return false 
+end
+
+return timer
